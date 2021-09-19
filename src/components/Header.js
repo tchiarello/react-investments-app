5bb8141f5db748606c81e6a568019fd9e3d06683
Tatiane Chiarello <tatichiarello@gmail.com>
2021-09-19 16:38:44 +0200

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Header() {
-  // const [name, setName] = useState('')
-  // event.currenttarget.value
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold text-white pt-7 pb-7 mb-7 bg-yellow-300">
+      <h1 className='text-center text-4xl font-bold text-white pt-7 pb-7 mb-7 bg-yellow-300'>
         React Investments App
       </h1>
     </div>
